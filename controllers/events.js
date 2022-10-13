7af9db5fbbf7381ccc94287ea1693c6758e4e829
Felix Miranda <feedmite@hotmail.com>
2022-10-12 23:36:32 -0500

    create events added

--- controllers/events.js
+++ controllers/events.js
@@ -1,16 +1,30 @@
 const { response } = require("express");
+const Evento = require("../models/Evento");
 
 const getEvento = (req, res = response) => {
   return res.json({ ok: true, message: "getEvento" });
 };
-const crearEvento = (req, res = response) => {
-  return res.json({ ok: true, message: "crearEvento" });
+const crearEvento = async (req, res = response) => {
+  const evento = new Evento(req.body);
+  evento.user = req.uid;
+  console.log({ evento });
+
+  try {
+    const eventoGuardado = await evento.save();
+    return res.status(200).json({ ok: true, evento: eventoGuardado });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      message: "Comunicarse con el administrador del servidor",
+    });
+  }
 };
 const actualizarEvento = (req, res = response) => {
   return res.json({ ok: true, message: "actualizarEvento" });
 };
 const eliminarEvento = (req, res = response) => {
-  return res.json({ ok: true, message: "getEvento" });
+  return res.json({ ok: true, message: "eliminarEvento" });
 };
 
 module.exports = {
--- routes/events.js
+++ routes/events.js
@@ -1,16 +1,40 @@
+/* Auth
+host + /api/events
+*/
+
 const { Router } = require("express");
+const { check } = require("express-validator");
 const {
   getEvento,
   crearEvento,
   actualizarEvento,
   eliminarEvento,
 } = require("../controllers/events");
+const { isDate } = require("../helpers/isDate");
+const { validarCampos } = require("../middlewares/validar-campos");
+const { validarJWT } = require("../middlewares/validar-jwt");
 
 const router = Router();
 
-router.get("/", getEvento);
-router.post("/", crearEvento);
-router.put("/", actualizarEvento);
-router.delete("/", eliminarEvento);
+// para validar JWT en todas las rutas
+router.use(validarJWT);
+
+router.get(
+  "/",
+
+  getEvento
+);
+router.post(
+  "/",
+  [
+    check("title", "El título es obligatorio").not().isEmpty(),
+    check("start", "La fecha de inicio es obligatoria").custom(isDate),
+    check("end", "La fecha de finalización es obligatoria").custom(isDate),
+    validarCampos,
+  ],
+  crearEvento
+);
+router.put("/:id", actualizarEvento);
+router.delete("/:id", eliminarEvento);
 
 module.exports = router;
